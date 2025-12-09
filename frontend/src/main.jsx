e3b320163dbe49d946c058b635fca80ccee26ae8
Anuj2275 <iamanujsanjeet@gmail.com>
2025-12-09 20:50:40 +0530

    strict mode removed from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,11 +7,11 @@ import {BrowserRouter} from 'react-router-dom'
 
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  // <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>
+  // </StrictMode>
 );
